9df76c8b40e256fe056c4e8e4e30868b7c9f52d5
Hewerton Adao da Paz <hewerton.paz@compasso.com.br>
2024-04-14 18:15:01 -0300

    add seed

---
+++ src/prisma/migrations/20240414203308_init/migration.sql
@@ -0,0 +1,115 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `avatarBgColor` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Expense` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `iconName` VARCHAR(191) NULL,
+    `amount` DOUBLE NULL,
+    `isPaid` BOOLEAN NOT NULL DEFAULT false,
+    `dueDate` DATETIME(3) NULL,
+    `registrationDate` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `dueDay` INTEGER NULL,
+    `registrationDay` INTEGER NULL,
+    `creditCardId` VARCHAR(191) NULL,
+    `userId` VARCHAR(191) NOT NULL,
+    `isRecurrent` BOOLEAN NOT NULL,
+    `frequency` ENUM('DAILY', 'MONTHLY', 'YEARLY') NULL,
+    `totalInstallments` INTEGER NULL,
+    `paidInstallments` INTEGER NULL,
+    `paymentType` ENUM('CREDIT_CARD', 'DEBIT_CARD', 'CASH', 'PIX') NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `CreditCard` (
+    `id` VARCHAR(191) NOT NULL,
+    `iconName` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `dueDay` INTEGER NOT NULL,
+    `registrationDay` INTEGER NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Category` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `SubCategory` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NULL,
+    `categoryId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `expenseId` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ExpenseSubCategory` (
+    `expenseId` VARCHAR(191) NOT NULL,
+    `subCategoryId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`expenseId`, `subCategoryId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TransitionHistory` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `amount` DOUBLE NULL,
+    `expenseId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Expense` ADD CONSTRAINT `Expense_creditCardId_fkey` FOREIGN KEY (`creditCardId`) REFERENCES `CreditCard`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `CreditCard` ADD CONSTRAINT `CreditCard_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `SubCategory` ADD CONSTRAINT `SubCategory_categoryId_fkey` FOREIGN KEY (`categoryId`) REFERENCES `Category`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `SubCategory` ADD CONSTRAINT `SubCategory_expenseId_fkey` FOREIGN KEY (`expenseId`) REFERENCES `Expense`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ExpenseSubCategory` ADD CONSTRAINT `ExpenseSubCategory_expenseId_fkey` FOREIGN KEY (`expenseId`) REFERENCES `Expense`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ExpenseSubCategory` ADD CONSTRAINT `ExpenseSubCategory_subCategoryId_fkey` FOREIGN KEY (`subCategoryId`) REFERENCES `SubCategory`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `TransitionHistory` ADD CONSTRAINT `TransitionHistory_expenseId_fkey` FOREIGN KEY (`expenseId`) REFERENCES `Expense`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/prisma/seed.ts
@@ -0,0 +1,231 @@
+import prisma from "../lib/prisma";
+import { faker } from "@faker-js/faker";
+import { hash } from "bcrypt";
+import { getRandomRGBColor } from "../shared/colors";
+import { Prisma } from "@prisma/client";
+import { cuid } from "../shared/cuid";
+
+export async function main() {
+  const subCategoryRentId = cuid();
+  const subCategoryIfooId = cuid();
+  const subCategoryRestaurantId = cuid();
+  const subCategoryInfoProdictGitfitCardId = cuid();
+  const categories = [
+    {
+      name: "Food",
+      subCategories: [
+        { name: "Supermarket" },
+        { id: subCategoryRestaurantId, name: "Restaurant" },
+      ],
+    },
+    {
+      name: "Services",
+      subCategories: [
+        { name: "Cleaning" },
+        { name: "Laundry" },
+        { name: "Electrician" },
+        { name: "Funerary" },
+        { name: "Mason" },
+        { name: "MEI" },
+        { name: "Plumber" },
+        { name: "Gardener" },
+        { name: "Internet" },
+        { name: "Cell phone" },
+        { id: subCategoryIfooId, name: "Ifood" },
+      ],
+    },
+    {
+      name: "Subscriptions",
+      subCategories: [
+        { name: "Streamings" },
+        { name: "Musics Subscriptions" },
+        { name: "Games Subscriptions" },
+      ],
+    },
+    {
+      name: "Transport",
+      subCategories: [{ name: "Uber" }, { name: "Bus" }],
+    },
+    {
+      name: "Housing",
+      subCategories: [{ id: subCategoryRentId, name: "Rent" }],
+    },
+    {
+      name: "Health",
+      subCategories: [{ name: "Medicine" }, { name: "Dentist" }],
+    },
+    {
+      name: "Aesthetic",
+      subCategories: [
+        { name: "Beard and Hair" },
+        { name: "Nails" },
+        { name: "Skin" },
+      ],
+    },
+    {
+      name: "Leisure",
+      subCategories: [{ name: "Cinema" }, { name: "Travel" }],
+    },
+    {
+      name: "Knowledge/Education",
+      subCategories: [
+        { name: "Book" },
+        { name: "Magazine" },
+        { name: "Newspaper" },
+        { name: "English" },
+        { name: "Course" },
+        { name: "Ebook" },
+      ],
+    },
+    {
+      name: "Info products",
+      subCategories: [
+        { name: "Gift Card", id: subCategoryInfoProdictGitfitCardId },
+        { name: "Games" },
+      ],
+    },
+    {
+      name: "Esportes",
+      subCategories: [
+        { name: "Gym" },
+        { name: "Crossfit" },
+        { name: "Swimming" },
+        { name: "Soccer" },
+        { name: "Hit" },
+      ],
+    },
+    {
+      name: "Pets",
+      subCategories: [{ name: "Pets Medicine" }, { name: "Food" }],
+    },
+    {
+      name: "Clothing",
+      subCategories: [
+        { name: "Shoes" },
+        { name: "T-Shirts" },
+        { name: "Pants" },
+        { name: "Shoes" },
+      ],
+    },
+    {
+      name: "Home",
+      subCategories: [
+        { name: "Furniture" },
+        { name: "Decoration" },
+        { name: "Home appliances" },
+        { name: "gas" },
+        { name: "Electronics" },
+        { name: "Power and lighting" },
+        { name: "Water" },
+      ],
+    },
+    {
+      name: "Electronics",
+      subCategories: [
+        { name: "Smartphone" },
+        { name: "Computer" },
+        { name: "Video game" },
+      ],
+    },
+    {
+      name: "Supplements",
+      subCategories: [
+        { name: "Proteins" },
+        { name: "Vitamins" },
+        { name: "Creatine" },
+      ],
+    },
+    {
+      name: "Others",
+      subCategories: [{ name: "Other" }],
+    },
+  ];
+  const countUsers = await prisma.user.count();
+  if (countUsers) return;
+
+  for (const category of categories) {
+    console.log(category.subCategories);
+    await prisma.category.create({
+      data: {
+        name: category.name,
+        subCategories: { create: category.subCategories },
+      },
+    });
+  }
+
+  const password = await hash("123456789", 10);
+  const createdUser = await prisma.user.create({
+    data: {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      avatarBgColor: getRandomRGBColor(),
+      password,
+    },
+  });
+  const creditCardNubankId = cuid();
+  const creditCardRiachueloId = cuid();
+  await prisma.creditCard.createMany({
+    data: [
+      {
+        id: creditCardNubankId,
+        name: "Nubank",
+        dueDay: 4,
+        registrationDay: 29,
+        userId: createdUser.id,
+      },
+      {
+        id: creditCardRiachueloId,
+        name: "Riachuelo",
+        dueDay: 30,
+        registrationDay: 24,
+        userId: createdUser.id,
+      },
+    ],
+  });
+  const createExpenses: Prisma.ExpenseCreateArgs[] = [
+    {
+      data: {
+        name: "Aluguel",
+        amount: 450,
+        userId: createdUser.id,
+        paymentType: "PIX",
+        isRecurrent: true,
+        registrationDay: 1,
+        dueDay: 5,
+        subCategories: {
+          connect: { id: subCategoryRentId },
+        },
+      },
+    },
+    {
+      data: {
+        name: "Gift Card PlayStation Store R$100",
+        amount: 100,
+        userId: createdUser.id,
+        paymentType: "PIX",
+        isRecurrent: false,
+        isPaid: true,
+        subCategories: {
+          connect: { id: subCategoryInfoProdictGitfitCardId },
+        },
+      },
+    },
+    {
+      data: {
+        name: "ifood Ana Cleide Campos",
+        amount: 16.99,
+        userId: createdUser.id,
+        paymentType: "CREDIT_CARD",
+        isRecurrent: false,
+        subCategories: {
+          connect: [{ id: subCategoryIfooId }, { id: subCategoryRestaurantId }],
+        },
+      },
+    },
+  ];
+  for (const expense of createExpenses) {
+    await prisma.expense.create(expense);
+  }
+}
+
+main();
