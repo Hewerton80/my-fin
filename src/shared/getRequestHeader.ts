ea4049090d907a84fabe4fcc6882f5bff77fae1a
hewerton <hewerton80@gmail.com>
2024-07-15 13:49:40 -0300

    refactor: Update .gitignore and .env.example files

--- src/shared/getRequestHeader.ts
+++ src/shared/getRequestHeader.ts
@@ -2,5 +2,6 @@ import { getCurretToken } from "@/lib/cookie";
 
 export const getRequestHeaders = () => {
   const token = getCurretToken();
+  console.log({ token });
   return { Authorization: `Bearer ${token}` };
 };
